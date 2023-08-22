6cab9177e8e929d1baa414440ee73a36a21c5691
Shook <63891580+jashook828@users.noreply.github.com>
2023-08-21 23:56:20 -0500

    Fixed nesting issues in index.html

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 /* script.js file for Onboarding Project*/
+
+/*Here we are using the getElementByID() method to select the HTML elements from our
+HTML file and storing them in javascript elements.  This will allow us to use them
+without having to constantly call the method.*/
+var form = document.getElementById('todo-form');        //Allows user to interact with the form
+var input = document.getElementById('todo-input');      //Allows user to store list items ny using the button
+var list = document.getElementById('todo-list');        //Allows user to see the updated todo-list
+
+/*Here we are calling the addEventListener on our window.  When our webpage loads
+entirely the call back function will load our statement into our window and let us
+know that the event is completed.*/
+window.addEventListener('load', function() {
+    console.log('Page has loaded!');
+  });
+  
